54c579a037eaea5aff5dfaa48bf6443240ec66b9
a.ilyin <a.ilyin@smartcat.ai>
2026-04-24 11:08:49 +0400

    local

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Craft app configuration
+ *
+ * @see https://craftcms.com/docs/5.x/reference/config/app.html
+ */
+
+return [
+    'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
+];
---
+++ config/general.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\config\GeneralConfig;
+
+return GeneralConfig::create()
+    ->cpTrigger(getenv('CRAFT_CP_TRIGGER') ?: 'admin')
+    ->defaultWeekStartDay(1)
+    ->devMode(App::env('CRAFT_ENVIRONMENT') !== 'production')
+    ->allowAdminChanges(App::env('CRAFT_ENVIRONMENT') !== 'production')
+    ->disallowRobots(App::env('CRAFT_ENVIRONMENT') !== 'production');
